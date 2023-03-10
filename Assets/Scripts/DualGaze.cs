6fc3a118f1d131bbc243c7cc733ac4509829f680
Axel Goris <axelgoris99@gmail.com>
2023-03-10 16:13:29 +0100

    WIP - DualGaze : Projection of the points onto the plane, need to calculate the convex hull and find the closest point on this full for the flag to appear

--- Assets/Scripts/DualGaze.cs
+++ Assets/Scripts/DualGaze.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class DualGaze : MonoBehaviour
 {
@@ -9,18 +11,20 @@ public class DualGaze : MonoBehaviour
     public static event SelectionDualGaze onSelected;
 
     [SerializeField] private GameObject objectToSpawn;
-    [SerializeField] private float distFlag;
+    public GameObject sphere;
     // Think about the layers to setup in unity !!
     int layerMask = 1;
     int layerMaskFlag = 8;
+    int layerMaskPlane = 7;
     private int layerToInteractWith;
     GameObject selectedObject;
     GameObject flag;
     bool grabbed = false;
     [SerializeField] float timerRef = 2.0f;
     float timer = 2.0f;
-
-
+    public GameObject camPlane;
+    Ray ray;
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +34,7 @@ public class DualGaze : MonoBehaviour
         layerToInteractWith = 6;
         layerMask = 1 << layerToInteractWith;
         layerMaskFlag = 1 << 8;
+        layerMaskPlane = 1 << 7;     
 
     }
     private void OnDisable()
@@ -38,31 +43,59 @@ public class DualGaze : MonoBehaviour
         Grab.onRelease -= releasedObject;
     }
 
+    IEnumerator WaitForEndOfFrameSoThatThePlaneHasTimeToMove()
+    {
+        // suspend until the end
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        Debug.Log("test");
+        BoxCollider bounds = selectedObject.GetComponent<BoxCollider>();
+
+        Vector3[] corners = new Vector3[8];
+        corners[0] = bounds.center + new Vector3(bounds.size.x / 2, bounds.size.y / 2, bounds.size.z / 2);
+        corners[1] = bounds.center + new Vector3(bounds.size.x / 2, bounds.size.y / 2, -bounds.size.z / 2);
+        corners[2] = bounds.center + new Vector3(bounds.size.x / 2, -bounds.size.y / 2, bounds.size.z / 2);
+        corners[3] = bounds.center + new Vector3(bounds.size.x / 2, -bounds.size.y / 2, -bounds.size.z / 2);
+        corners[4] = bounds.center + new Vector3(-bounds.size.x / 2, bounds.size.y / 2, bounds.size.z / 2);
+        corners[5] = bounds.center + new Vector3(-bounds.size.x / 2, bounds.size.y / 2, -bounds.size.z / 2);
+        corners[6] = bounds.center + new Vector3(-bounds.size.x / 2, -bounds.size.y / 2, bounds.size.z / 2);
+        corners[7] = bounds.center + new Vector3(-bounds.size.x / 2, -bounds.size.y / 2, -bounds.size.z / 2);
+
+        Vector3[] worldPosition = new Vector3[8];
+        for (int i = 0; i < corners.Length; i++)
+        {
+            worldPosition[i] = selectedObject.transform.TransformPoint(corners[i]);
+        }
+        for (int i = 0; i < corners.Length; i++)
+        {
+            RaycastHit hitPlane;
+            Ray rayVertex = new Ray(ray.origin, worldPosition[i] - ray.origin);
+            if (Physics.Raycast(rayVertex, out hitPlane, Mathf.Infinity, layerMaskPlane))
+            {
+                flag = Instantiate(sphere);
+                flag.transform.position = hitPlane.point;
+                flag.SetActive(true);
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+        ray = RayCastingSelector.Instance.ray;
         if (grabbed) { return; }
-        Ray ray = RayCastingSelector.Instance.ray;
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
             timer = timerRef;
-            if (selectedObject == null)
+            if (selectedObject != hit.transform.gameObject)
             {
                 selectedObject = hit.transform.gameObject;
-                Vector3 hitPoint = hit.point;
-                // In case the flag is still up 
-                if(flag != null) { Destroy(flag); }
-                flag = Instantiate(objectToSpawn);
-                flag.transform.position = hitPoint + hit.normal.normalized * flag.GetComponent<BoxCollider>().size.z * distFlag;
-                flag.SetActive(true);
-            }
-            if(hit.transform.gameObject != selectedObject && hit.transform.gameObject != flag)
-            {
-                // We could change it here but we can just wait for one more frame, doesn't change much
-                selectedObject = null;
+                camPlane.transform.position = selectedObject.transform.position;
+                StartCoroutine(WaitForEndOfFrameSoThatThePlaneHasTimeToMove());
             }
         }
+        
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMaskFlag))
         {
             if (hit.transform.gameObject == flag)
@@ -74,6 +107,7 @@ public class DualGaze : MonoBehaviour
         timer -= Time.deltaTime;
         if(timer < 0.0f)
         {
+            selectedObject = null;
             Destroy(flag);
         }
     }
